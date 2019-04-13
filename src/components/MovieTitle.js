87957f84fc4fb88201b7188833491e198f708846
unknown <islam.sayed@ISLAM-SAYED-REP.EL-EZABYGROUP.LOCAL>
2019-04-13 17:18:05 +0200

    format code

--- src/components/MovieTitle.js
+++ src/components/MovieTitle.js
@@ -4,23 +4,23 @@ import StarRate from "@material-ui/icons/StarRate";
 import Icon from "@material-ui/core/Icon";
 import "../Animate.css";
 
-const MovieTitle = (props) => {
-        const { rating, movieTitle } = props
-        return (
-            <h2 className="h4 animated fadeIn">
-                {movieTitle}
-                {"  "}
-                <Chip
-                  style={{ background: "#000", color: "#fff" }}
-                  icon={
-                    <Icon fontSize="small" style={{ color: "gold" }}>
-                      <StarRate className="animated fadeIn slow" />
-                    </Icon>
-                  }
-                  label={rating}
-                />
-              </h2>
-        )
-}
+const MovieTitle = props => {
+  const { rating, movieTitle } = props;
+  return (
+    <h2 className="h4 animated fadeIn">
+      {movieTitle}
+      {"  "}
+      <Chip
+        style={{ background: "#000", color: "#fff" }}
+        icon={
+          <Icon fontSize="small" style={{ color: "gold" }}>
+            <StarRate className="animated fadeIn slow" />
+          </Icon>
+        }
+        label={rating}
+      />
+    </h2>
+  );
+};
 
-export default MovieTitle
+export default MovieTitle;
