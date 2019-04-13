3182abd12dfddeb96126257e115728bf8f0d38cc
unknown <islam.sayed@ISLAM-SAYED-REP.EL-EZABYGROUP.LOCAL>
2019-04-13 17:18:26 +0200

    Create fav button

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -4,36 +4,40 @@ import "../Animate.css";
 import { withStyles } from "@material-ui/core/styles";
 import Favorite from "@material-ui/icons/Favorite";
 import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+import IconButton from "@material-ui/core/IconButton";
+
 const styles = theme => ({
-    img: {
-      height: "100%",
-      maxWidth: "100%"
-    }
-  });
+  img: {
+    height: "100%",
+    maxWidth: "100%"
+  }
+});
 
 class Poster extends Component {
+  handleClick = () => this.props.toggleLike();
 
-
-  handleClick = () => this.props.toggleLike()
-
-    render() {
-        const { poster, movieTitle, isLiked } = this.props
-        return (
-            <div className="poster-wrapper">
-               <img
-                  src={poster}
-                  className="img-fluid mt-xs-2 mt-sm-4 mt-lg-5 poster animated fadeIn fast"
-                  alt={movieTitle}
-                />
-                 {isLiked === "liked" ? (
-                  <Favorite color="secondary" className="cliked-fav-icon animated fadeIn slow" onClick={this.handleClick} />
-                ): isLiked === "unliked" ? (
-                  <FavoriteBorder color="secondary" className="fav-icon animated fadeIn slow" onClick={this.handleClick} />
-                ): null}
-                  
-            </div>
-        )
-    }
+  render() {
+    const { poster, movieTitle, isLiked } = this.props;
+    return (
+      <div className="poster-wrapper">
+        <img
+          src={poster}
+          className="img-fluid mt-xs-2 mt-sm-4 mt-lg-5 poster animated fadeIn fast"
+          alt={movieTitle}
+        />
+        <IconButton
+          className="fav-icon animated fadeIn slow"
+          onClick={this.handleClick}
+        >
+          {isLiked === "liked" ? (
+            <Favorite color="secondary" />
+          ) : isLiked === "unliked" ? (
+            <FavoriteBorder color="secondary" />
+          ) : null}
+        </IconButton>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(Poster);
