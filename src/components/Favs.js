4cfb58d9a6ee78d024713f6363259f6d40f26b7e
unknown <islam.sayed@ISLAM-SAYED-REP.EL-EZABYGROUP.LOCAL>
2019-04-13 17:16:56 +0200

    create component

--- src/components/Favs.js
+++ src/components/Favs.js
@@ -1,50 +1,85 @@
-import React, {Component} from "react";
-import Grid from "@material-ui/core/Grid";
+import React, { Component } from "react";
+import Favorite from "@material-ui/icons/Favorite";
+import "../App.css";
 
-class Favs extends Component  {
-    componentDidMount() {
-        
-        (setTimeout(() => {return (this.props.favedMoviesIds).map(movieId => fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=6528ff68dbc27d13fb177793f4c69f9d&language=en-US`)
-                                                                            .then(response => response.json())
-                                                                            .then(data => this.getPosters(data.poster_path)))}, 3000))/* .map(movieId => fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=6528ff68dbc27d13fb177793f4c69f9d&language=en-US`)
-                                                .then(response => response.json())
-                                                .then(data => this.getPoster(data.poster_path))) */
-    }
-    state = {
-        posters: [],
-    }
-    
-    getPosters = (posterPath) => {
-        /* this.setState(prevState => ({
-            posters: prevState.posters.concat(`https://image.tmdb.org/t/p/w200/${posterPath}`)
-        })) */
-        this.setState(prevState => ({
-            posters: prevState.posters.concat(`https://image.tmdb.org/t/p/w200/${posterPath}`)
-        }))
-        console.log(`https://image.tmdb.org/t/p/w200/${posterPath}`)
-    }
-
-    render() {       
-        const { posters } = this.state
-        return (
-            <div className="container py-5 w-100">
-                <div className="row justify-content-center align-items-center no-gutters">
-                
-            {posters.map(poster => (
-                <div key={poster} className="col-12 col-md-3 text-center">
-                <img
-                  src={poster}
-                  className="border border-danger w-100 animated fadeIn fast"
-                  alt=""
-                />
-                </div>
-            ))}
-            </div>
-                </div>
-
+class Favs extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      return this.props.favedMoviesIds.map(movieId =>
+        fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}?api_key=6528ff68dbc27d13fb177793f4c69f9d&language=en-US`
         )
+          .then(response => response.json())
+          .then(data =>
+            this.getMovieDetails(data.poster_path, movieId, data.title)
+          )
+          .catch(error => {
+            console.log(error);
+            const linkToFailedRequest = document.getElementById(
+              "failed-request"
+            );
+            linkToFailedRequest.click();
+          })
+      );
+    }, 3000);
+  }
+
+  state = {
+    movies: []
+  };
+
+  handleClick = e => {
+    const targetId = Number(e.target.parentElement.id);
+    this.setState(prevState => ({
+      movies: prevState.movies.filter(movie => movie.movieId !== targetId)
+    }));
+    if (!e.target.id) {
+      console.log(typeof e.target.parentElement.id);
+      this.props.unlikeFav(e.target.parentElement.id);
+    } else {
+      this.props.unlikeFav(e.target.id);
+      console.log(e.target.id);
     }
-        
+  };
+  getMovieDetails = (posterPath, movieId, title) => {
+    this.setState(prevState => ({
+      movies: prevState.movies.concat({
+        posterPath: `https://image.tmdb.org/t/p/w200/${posterPath}`,
+        movieId,
+        title
+      })
+    }));
+  };
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div className="container-fluid py-5 w-100 fav-wrapper">
+        <div className="row justify-content-around no-gutters">
+          {movies.map(movie => (
+            <div
+              key={movie.movieId}
+              className="col-12 col-md-4 text-center mb-3 mb-md-2 movie-card"
+            >
+              <img
+                src={movie.posterPath}
+                className="w-100 h-100 animated fadeIn fast"
+                alt={movie.title}
+                title={movie.title}
+                data-id={movie.movieId}
+              />
+              <Favorite
+                color="secondary"
+                id={movie.movieId}
+                onClick={this.handleClick}
+                className="fav-icon animated fadeIn slow"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Favs
+export default Favs;
